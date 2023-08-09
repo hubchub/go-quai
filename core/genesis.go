0268139bc132fb41dcd89578e0c74b6ccbf70444
Djadih <13135116+Djadih@users.noreply.github.com>
2023-08-09 11:32:38 -0500

    Raise local difficulty

--- core/genesis.go
+++ core/genesis.go
@@ -428,7 +428,7 @@ func DefaultGalenaGenesisBlock(consensusEngine string) *Genesis {
 		Nonce:      0,
 		ExtraData:  hexutil.MustDecode("0x3535353535353535353535353535353535353535353535353535353535353537"),
 		GasLimit:   5000000,
-		Difficulty: big.NewInt(1000),
+		Difficulty: big.NewInt(100000000),
 	}
 }
 
@@ -448,7 +448,7 @@ func DefaultLocalGenesisBlock(consensusEngine string) *Genesis {
 		Nonce:      0,
 		ExtraData:  hexutil.MustDecode("0x3535353535353535353535353535353535353535353535353535353535353535"),
 		GasLimit:   5000000,
-		Difficulty: big.NewInt(1000),
+		Difficulty: big.NewInt(100000000),
 	}
 }
 
